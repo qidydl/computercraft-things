653564574f429dbd9aafe757afb5d7fcbcca8948
David Osolkowski <qidydl@gmail.com>
2013-08-19 19:35:19 -0400

    Initial version of ComputerCraft event API

---
+++ libccevent.lua
@@ -0,0 +1,37 @@
+-- libccevent.lua
+-- lib-ComputerCraft-event
+--
+-- A library for registering ComputerCraft event handlers and processing them in order.
+-- Event handler callbacks should return 'true' to indicate that the event has been handled, or 'false' to continue
+-- processing with the next event handler.
+--
+
+os.loadAPI("libccclass")
+
+ccEvent = class(function()
+	-- Constructor
+	self._handlers = {}
+end)
+
+function ccEvent:register(event, callback)
+	-- If nobody's registered for this event yet, initialize to empty handler list
+	if self._handlers[event] == nil then
+		self._handlers[event] = {}
+	end
+
+	-- Add handler to the end of the list
+	table.insert(self._handlers[event], callback)
+end
+
+function ccEvent:doEventLoop()
+	while true do
+		local result = {os.pullEvent()}
+		local event = result[0]
+
+		if self._handlers[event] ~= nil then
+			for handler in self._handlers[event] do
+				if handler(unpack(result)) then break end
+			end
+		end
+	end
+end
