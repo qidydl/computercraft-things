507ceb62800e6af366a989cfdd15f99acce81865
David Osolkowski <qidydl@gmail.com>
2013-08-20 10:50:10 -0400

    Add periodic timer and debug flag
    Debug flag controls whether output is printed to the console--leave off
    if not needed to save resources.
    
    Periodic timer allows events every second--will be extended to handle
    different durations.

--- libccevent.lua
+++ libccevent.lua
@@ -8,13 +8,17 @@
 
 os.loadAPI("libccclass")
 
-ccEvent = libccclass.class(function(cce)
+ccEvent = libccclass.class(function(cce, debugFlag)
 	-- Constructor
 	cce._handlers = {}
+	cce._debugFlag = debugFlag
+	cce._periodicTimerID = -1
 end)
 
 function ccEvent:register(event, callback)
-	print("Registering handler for " .. event)
+	if (self._debugFlag) then
+		print("Registering handler for " .. event)
+	end
 	-- If nobody's registered for this event yet, initialize to empty handler list
 	if self._handlers[event] == nil then
 		self._handlers[event] = {}
@@ -23,18 +27,37 @@ function ccEvent:register(event, callback)
 	-- Add handler to the end of the list
 	table.insert(self._handlers[event], callback)
 
-	print("There are now " .. #self._handlers[event] .. " handlers for " .. event)
+	if (self._debugFlag) then
+		print("There are now " .. #self._handlers[event] .. " handlers for " .. event)
+	end
 end
 
 function ccEvent:doEventLoop()
+	-- Kick off periodic timer
+	self._periodicTimerID = os.startTimer(1.0)
 	while true do
 		local result = {os.pullEvent()}
-		print("Event: [" .. table.concat(result, "] [") .. "]")
 		local event = result[1]
 
-		if self._handlers[event] ~= nil then
+		if (self._debugFlag) then
+			print("Event: [" .. table.concat(result, "] [") .. "]")
+		end
+
+		-- Special processing for our periodic timer
+		if (event == "timer" and result[2] == self._periodicTimerID) then
+			-- Reset the timer
+			self._periodicTimerID = os.startTimer(1.0)
+			
+			-- Call *all* timer handlers - different from default behavior
+			if self._handlers["periodic_timer"] ~= nil then
+				parallel.waitForAll(unpack(self._handlers["periodic_timer"]))
+			end
+		elseif self._handlers[event] ~= nil then
 			for k,handler in pairs(self._handlers[event]) do
-				print("Trying handler " .. tostring(k) .. ": " .. tostring(handler))
+				if (self._debugFlag) then
+					print("Trying handler " .. tostring(k) .. ": " .. tostring(handler))
+				end
+
 				if handler(unpack(result)) then break end
 			end
 		end
