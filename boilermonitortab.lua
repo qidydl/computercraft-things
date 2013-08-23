20912a5f6b3355d9de3290acec31068f5e3c3099
David Osolkowski <qidydl@gmail.com>
2013-08-23 07:46:31 -0400

    Fleshing out boiler monitor application
    Add actual boiler monitor object and register it.
    
    Save boiler type.
    
    Add display function and start with rendering.

--- boilermonitortab.lua
+++ boilermonitortab.lua
@@ -7,34 +7,53 @@ os.loadAPI("libccevent")
 os.loadAPI("libcctabs")
 os.loadAPI("librcboiler")
 
-BoilerMonitorTab = libccclass.class(function (self, boilerID, boilerName, tabs)
+-- Boiler types: 0 = solid-fueled, 1 = liquid-fueled
+BoilerMonitorTab = libccclass.class(function (self, side, boilerID, boilerName, boilerType)
+	self._networkSide = side
 	self._boilerID = boilerID
 	self._boilerName = boilerName
-	self._tabDialog = tabs
+	self._boilerType = boilerType
+
+	self._boilerMonitor = librcboiler.BoilerMonitor(side, boilerID, boilerType)
 end)
 
-function BoilerMonitorTab:registerTab()
+function BoilerMonitorTab:registerTab(tabs)
+	self._tabDialog = tabs
 	self._tabID = self._tabDialog:addTab(self._boilerName, function(tabs, visible)
 		-- Tab display callback
 		if visible then
-			-- Display stuff
+			self:display()
 		else
-			-- Hide stuff
+			-- Hide stuff if necessary - screen will be wiped but buttons and such need to have status updated
 		end
 	end)
 end
 
+function BoilerMonitorTab:display()
+	local monitor = self._tabDialog.monitor
+
+	-- Display boiler information
+	monitor.setCursorPos(1, 5)
+	monitor.write("Boiler Monitor Tab for " .. self._boilerName)
+	monitor.setCursorPos(1, 6)
+	monitor.write("Boiler Temperature: " .. self._boilerMonitor._state.temperature .. " C")
+end
+
 function BoilerMonitorTab:registerWith(cce)
+	self._boilerMonitor:registerWith(cce)
+
 	cce:register("railcraft_boiler", function(event, boilerID, boilerEvent, param)
 		if (boilerID == self._boilerID) then
 			-- Handle events
+			print("BMTab received [" .. event .. "] [" .. boilerID .. "] [" .. boilerEvent .. "]")
+
 			if (self._tabDialog._selectedTab == self._tabID) then
-				-- Display stuff
+				self:display()
 			else
 				-- Highlight if necessary
 				if (boilerEvent:sub(-3) == "low") then
 					self._tabDialog:highlightTab(self._tabID, "warning")
-				elseif (boilerEvent:sub(-3) == "critical") then
+				elseif (boilerEvent:sub(-8) == "critical") then
 					self._tabDialog:highlightTab(self._tabID, "danger")
 				end
 			end
